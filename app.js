3d08729084762675cea641be1c2c904b26ab64a3
SolMalnis <solmalnis@gmail.com>
2021-09-28 16:59:58 -0300

    mercado liebre v.01

--- app.js
+++ app.js
@@ -8,12 +8,27 @@ app.use(express.static(publicPath))
 
 
 app.listen(5050, ()=>{
-    console.log('funciona en http://localhost:5050/' )
+    console.log('funciona en http://localhost:5050' )
 })
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
 
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+})
+
+app.post('/login',(req,res)=>{
+    res.redirect("/")
+})
+
+app.post('/register',(req,res)=>{
+    res.redirect("/")
+})
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
 
 
